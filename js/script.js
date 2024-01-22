04e2750057402d45493112321ca59db56380e0ac
thauanb11 <guedesbruno40@gmail.com>
2024-01-22 20:06:50 -0300

    Portfolio finalizado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+
+
+
 let sections = document.querySelectorAll('section')
 let navLinks = document.querySelectorAll('header nav a')
 
@@ -18,4 +30,28 @@ window.onscroll = () => {
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 
+
+    menuIcon.classList.remove('bx-x')
+    navbar.classList.remove('active')
 };
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1 ', { origin: 'left' });
+ScrollReveal().reveal('.home-content p , .about-content', { origin: 'right' });
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ['FullStack Developer', 'Cloud Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
